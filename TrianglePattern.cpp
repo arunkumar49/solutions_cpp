2766c8de8c11a810f5358dc858f4ff43ba47c66d
Arun Kumar P <98594129+arunkumar49@users.noreply.github.com>
2022-03-15 15:34:39 +0530

    Successfully printed right angled triangle with stars

--- TrianglePattern.cpp
+++ TrianglePattern.cpp
@@ -1,1 +1,25 @@
+#include<iostream>
 
+using namespace std;
+
+
+void print_right_star(int num);      //Prototyping
+
+int main(){
+   int num{};
+   
+   cout << "Enter the number of rows you want:";
+   cin >> num;
+
+   print_right_star(num);
+}
+
+
+void print_right_star(int num){ 
+   for(int i{1}; i<=num; i++){         // for printing rows
+      for(int j{1}; j<=i; j++){        // for printing columns
+         cout << "*";
+      }
+      cout << endl;
+   }  
+}
